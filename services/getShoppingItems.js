374ae7a08c2c0236a351a4c5033157d4f242168d
LatinGeek <germanlamelar@hotmail.com>
2023-09-27 14:57:14 -0300

    [CHORE] Create getShoppingItems in Service

---
+++ services/getShoppingItems.js
@@ -0,0 +1,15 @@
+
+const HARDCODED_SHOPPING_ITEMS = [
+    {
+        thumbnailImg: "",
+        title: "",
+        price: 0,
+        description: ""
+    }
+]
+
+export default function getShoppingItems() {
+    //TODO: Fetch shopping items from API
+    
+
+}
