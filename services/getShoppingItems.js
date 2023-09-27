97e51811df35d34de05d38f29a80658bb41761eb
LatinGeek <germanlamelar@hotmail.com>
2023-09-27 15:04:45 -0300

    [CHORE] Hardcode all test shopping items

--- services/getShoppingItems.js
+++ services/getShoppingItems.js
@@ -2,10 +2,33 @@
 const HARDCODED_SHOPPING_ITEMS = [
     {
         title: "Tourmaline & Eucalyptus Bar Soap",
+        id: 1,
         thumbnailImg: "",
         price: 12.00,
         description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
-    }
+    },
+    {
+        title: "Tourmaline & Argan Oil Bar Soap",
+        id: 2,
+        thumbnailImg: "",
+        price: 12.00,
+        description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
+    },
+    {
+        title: "Tourmaline & Tea Tree Bar Soap",
+        id: 3,
+        thumbnailImg: "",
+        price: 12.00,
+        description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
+    },
+    {
+        title: "Tourmaline Unscented Bar Soap",
+        id: 4,
+        thumbnailImg: "",
+        price: 12.00,
+        description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
+    },
+
 ]
 
 export default function getShoppingItems() {
