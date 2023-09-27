61b79e002315c5c8321beaca4ea82fcb8e0d7e9f
LatinGeek <germanlamelar@hotmail.com>
2023-09-27 15:00:17 -0300

    [CHORE] Add first hardcoded shopping item

--- services/getShoppingItems.js
+++ services/getShoppingItems.js
@@ -1,15 +1,15 @@
 
 const HARDCODED_SHOPPING_ITEMS = [
     {
+        title: "Tourmaline & Eucalyptus Bar Soap",
         thumbnailImg: "",
-        title: "",
-        price: 0,
-        description: ""
+        price: 12.00,
+        description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
     }
 ]
 
 export default function getShoppingItems() {
     //TODO: Fetch shopping items from API
-    
+
 
 }
