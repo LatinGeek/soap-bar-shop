6882e82071b5553f642f85cb89ea9dc41c0ada84
LatinGeek <germanlamelar@hotmail.com>
2023-09-28 01:28:32 -0300

    [FEATURE] Define a maximum order quantity for each item / adjust quantity input for more than 2 digits

--- components/PriceSection.js
+++ components/PriceSection.js
@@ -3,20 +3,22 @@ import styles from '../styles/PriceSection.module.css'
 import { useState } from 'react';
 
 export default function PriceSection({ unitPrice, quantity }) {
+    //TODO: Define max amount of items to buy at once.
+    const MAX_AMOUNT = 1000;
+    const [getQuantity, updateQuantity] = useState(quantity);
 
     function handleValueUpdate(event) {
-        if (event.target.value >= 0) {
+        if (event.target.value >= 0 && event.target.value <= MAX_AMOUNT) {
             updateQuantity(event.target.value);
         }
     }
 
-    const [getQuantity, updateQuantity] = useState(quantity);
 
 
     return (
         <div className={styles.priceSection}>
             <h3 className={styles.priceTag}>${(unitPrice * getQuantity).toFixed(2)}</h3>
-            <input onChange={handleValueUpdate} className={styles.quantityInput} value={getQuantity} type='number' />
+            <input onChange={handleValueUpdate} size={getQuantity.length} className={[styles.quantityInput, getQuantity.length > 2 ? styles.inputLarge : styles.inputSmall].join(' ')} value={getQuantity} type='number' />
         </div>
     )
 }
