7479b57c0bc009922f24b9cc7f6cadf820b3043c
LatinGeek <germanlamelar@hotmail.com>
2023-09-28 00:20:59 -0300

    [FEATURE] Make ShoppingListItem dynamic

--- components/ShoppingListItem.js
+++ components/ShoppingListItem.js
@@ -2,16 +2,16 @@ import Image from 'next/image';
 import styles from '../styles/ShoppingListItem.module.css'
 import PriceSection from './PriceSection';
 
-export default function ShoppingListItem() {
+export default function ShoppingListItem({ title, description, price, thumbnailImg }) {
     return (
         <div className={styles.container}>
-            <Image alt='thumbnail' layout='responsive' width={500} height={200} src={"/images/test-soap-thumbnails/tourmaline-&-argan-oil-bar-soap.png"}></Image>
-            <h3>Tourmaline & Eucalyptus Bar Soap</h3>
+            <Image alt='thumbnail' layout='responsive' width={500} height={200} src={thumbnailImg}></Image>
+            <h3>{title}</h3>
 
-            <PriceSection unitPrice={12.00} quantity={1} />
+            <PriceSection unitPrice={price} quantity={1} />
 
             <div className={styles.descriptionSection}>
-                Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.
+                {description}
             </div>
 
             <div className={styles.actionSection}>
