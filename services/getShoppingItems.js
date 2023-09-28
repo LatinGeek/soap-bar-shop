0b7870e298f02866b5cc7bc63c47a5c5c8de4483
LatinGeek <germanlamelar@hotmail.com>
2023-09-28 00:29:52 -0300

    [FEATURE] Retrieve all shopping items from service

--- services/getShoppingItems.js
+++ services/getShoppingItems.js
@@ -3,42 +3,42 @@ const HARDCODED_SHOPPING_ITEMS = [
     {
         title: "Tourmaline & Eucalyptus Bar Soap",
         id: 1,
-        thumbnailImg: "",
+        thumbnailImg: "/images/test-soap-thumbnails/tourmaline-&-eucalyptus-bar-soap.png",
         price: 12.00,
         description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
     },
     {
         title: "Tourmaline & Argan Oil Bar Soap",
         id: 2,
-        thumbnailImg: "",
+        thumbnailImg: "/images/test-soap-thumbnails/tourmaline-&-argan-oil-bar-soap.png",
         price: 12.00,
         description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
     },
     {
         title: "Tourmaline & Tea Tree Bar Soap",
         id: 3,
-        thumbnailImg: "",
+        thumbnailImg: "/images/test-soap-thumbnails/tourmaline-&-tea-tree-bar-soap.png",
         price: 12.00,
         description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
     },
     {
         title: "Tourmaline Unscented Bar Soap",
         id: 4,
-        thumbnailImg: "",
+        thumbnailImg: "/images/test-soap-thumbnails/tourmaline-unscented-bar-soap.png",
         price: 12.00,
         description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
     },
     {
         title: "Tourmaline & Tea Tree Bar Soap",
         id: 5,
-        thumbnailImg: "",
+        thumbnailImg: "/images/test-soap-thumbnails/tourmaline-&-tea-tree-bar-soap.png",
         price: 12.00,
         description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
     },
     {
         title: "Tourmaline Unscented Bar Soap",
         id: 6,
-        thumbnailImg: "",
+        thumbnailImg: "/images/test-soap-thumbnails/tourmaline-unscented-bar-soap.png",
         price: 12.00,
         description: `Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion.`
     },
