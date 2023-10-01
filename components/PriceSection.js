2e73267914492cc6f27e32cc42c5f4318f92c825
LatinGeek <germanlamelar@hotmail.com>
2023-10-01 16:18:25 -0300

    [CHORE] Make currency a constant

--- components/PriceSection.js
+++ components/PriceSection.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 export default function PriceSection({ unitPrice, quantity, handleQuantityUpdate }) {
     //TODO: Define max amount of items to buy at once.
     const MAX_AMOUNT = 1000;
+    const CURRENCY = '$';
     const [getQuantity, updateQuantity] = useState(quantity);
 
     function handleValueUpdate(event) {
@@ -17,7 +18,7 @@ export default function PriceSection({ unitPrice, quantity, handleQuantityUpdate
 
     return (
         <div className={styles.priceSection}>
-            <h3 className={styles.priceTag}>${(unitPrice * getQuantity).toFixed(2)}</h3>
+            <h3 className={styles.priceTag}>{CURRENCY}{(unitPrice * getQuantity).toFixed(2)}</h3>
             <input onChange={handleValueUpdate} size={getQuantity.length} className={[styles.quantityInput, getQuantity.length > 2 ? styles.inputLarge : styles.inputSmall].join(' ')} value={getQuantity} type='number' />
         </div>
     )
